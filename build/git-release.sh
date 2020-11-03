75e0b4b5fde1b6db1a932bf20f4a92a67b91756e
guojufeng <guojufeng@jd.com>
2020-11-03 23:04:17 +0800

    config: auto release and publish

---
+++ build/git-release.sh
@@ -0,0 +1,27 @@
+###
+ # @Author: @Guojufeng
+ # @Date: 2020-11-03 22:28:46
+ # @LastEditors: @Guojufeng
+ # @LastEditTime: 2020-11-03 22:47:19
+ # @FilePath: /Users/guojufeng/Documents/GitHub/xingorg1-ui/build/git-release.sh
+### 
+#!/usr/bin/env sh
+
+set -e
+
+if test -n "$(git status --porcelain)"; then
+  echo 'Unclean working tree. Commit or stash changes first.' >&2;
+  exit 128;
+fi
+
+if ! git fetch --quiet 2>/dev/null; then
+  echo 'There was a problem fetching your branch. Run `git fetch` to see more...' >&2;
+  exit 128;
+fi
+
+if test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then
+  echo 'Remote history differ. Please pull changes.' >&2;
+  exit 128;
+fi
+
+echo 'No conflicts.' >&2;
---
+++ build/release.sh
@@ -0,0 +1,48 @@
+###
+ # @Author: @Guojufeng
+ # @Date: 2020-11-03 22:26:56
+ # @LastEditors: @Guojufeng
+ # @LastEditTime: 2020-11-03 23:03:22
+ # @FilePath: /Users/guojufeng/Documents/GitHub/xingorg1-ui/build/release.sh
+### 
+#!/usr/bin/env sh
+set -e
+
+git checkout main
+git merge dev
+
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run dist
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin main
+  git push origin refs/tags/v$VERSION
+  git checkout dev
+  git rebase main
+  git push origin dev
+
+  if [[ $VERSION =~ "beta" ]]
+  then
+    npm publish --tag beta
+  else
+    npm publish
+  fi
+fi
+
+
+
+
+
