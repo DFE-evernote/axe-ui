54368c36403c9f44435a0628d371f9d52c6599fe
xing.org1^ <xing.org1@outlook.com>
2020-11-17 19:37:33 +0800

    Platform: beautify

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,15 +5,35 @@
   background: $bg-color;
   height: 200px;
   padding: 20px 100px;
+  .axe-title {
+    float: right;
+    width: 100px;
+    margin: -30px -20px;
+    font-size: 24px;
+    font-family: 'Eina03-Bold';
+  }
+  ul {
+    width: 130px;
+  }
   li {
+    cursor: pointer;
     margin-top: 10px;
     font-size: 14px;
+    &:hover {
+      color: #fff;
+    }
   }
 }
 </style>
 <template>
   <div class="axe-footer">
-    axe-ui
+    <div class="axe-title">
+      axe-ui
+      <div class="img">
+        <!-- <img src="@/assets/images/logo-small.svg" alt="axe-ui" /> -->
+        <img src="@/assets/images/logo.svg" alt="axe-ui" />
+      </div>
+    </div>
     <ul>
       <li>
         关于我们
