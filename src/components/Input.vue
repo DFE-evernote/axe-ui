40d4af45440b3ab1a7bfee1aef9dc57a2f00342d
Otto <fa@ijust.cc>
2020-11-22 23:00:44 +0800

    feat: completed Input Component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="area demo-input">
-    <h1>input 输入框</h1>
+    <div class="demo-title">input 输入框</div>
     <p>基本用法1</p>
     <axe-input class="my-axe-input" v-model="input" placeholder="请输入内容" />
   </div>
---
+++ tests/unit/input.spec.js
@@ -0,0 +1,22 @@
+//  单元测试后面补，有个报错没解决
+// import AxeInput from 'packages/input'
+// // import { expect } from 'chai'
+// import { mount } from '@vue/test-utils'
+// import { ref } from 'vue'
+// describe('input component', () => {
+//   it('input', () => {
+//     const box = mount({
+//       template: `<axe-input v-model="input" placeholder="请输入内容" />`,
+//       components: {
+//         AxeInput
+//       },
+//       setup() {
+//         const input = ref('')
+//         return {
+//           input
+//         }
+//       }
+//     })
+//     console.log(box)
+//   })
+// })
