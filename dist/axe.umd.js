824dd7574b3ecd5f5cb3dad9c743a4518a7bd7c2
xing.org1^ <xing.org1@outlook.com>
2020-12-04 01:04:58 +0800

    build: build 0.2.2

--- dist/axe.umd.js
+++ dist/axe.umd.js
@@ -1056,10 +1056,11 @@
         var __WEBPACK_AMD_DEFINE_FACTORY__,
           __WEBPACK_AMD_DEFINE_ARRAY__,
           __WEBPACK_AMD_DEFINE_RESULT__ // addapted from the document.currentScript polyfill by Adam Miller
-          // MIT license
-          // source: https://github.com/amiller-gh/currentScript-polyfill
+        // MIT license
+        // source: https://github.com/amiller-gh/currentScript-polyfill
 
-          // added support for Firefox https://bugzilla.mozilla.org/show_bug.cgi?id=1620505
+        // added support for Firefox https://bugzilla.mozilla.org/show_bug.cgi?id=1620505
+
         ;(function(root, factory) {
           if (true) {
             !((__WEBPACK_AMD_DEFINE_ARRAY__ = []),
