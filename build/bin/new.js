d410e2fd07e3e644562e41ffa0658f8f34528af3
Otto <fa@ijust.cc>
2020-12-06 14:43:16 +0800

    docs: 补充流程描述

--- build/bin/new.js
+++ build/bin/new.js
@@ -6,7 +6,17 @@ const path = require('path'),
 process.on('exit', code => {
   console.log(code)
 })
-// 检测参数
+
+/**
+ * 1. 判断异常：参数长度
+ * 2. 组合组件名
+ * 3. 判断异常：组件重名
+ * 4. 文件填充： /components.json
+ * 5. 文件填充： /styles/index.scss
+ * 6. 组合文件填充
+ */
+
+// 1.判断异常：参数长度
 if (process.argv.length < 3) {
   // 不传组件名，真是罪过～ 直接杀死！
   console.assert(
@@ -21,6 +31,7 @@ if (process.argv.length < 3) {
   )
 }
 
+// 2. 组合组件名
 const compName = process.argv[2],
   compChineseName = process.argv[3] || compName, // 获取传进来的组件中文名
   compNameCameraCase = uppercamelcase(compName), // 大驼峰
@@ -28,7 +39,7 @@ const compName = process.argv[2],
   compPath = path.resolve(__dirname, '../../packages/', compName)
 console.log(compPath)
 
-// 检测重名
+// 3. 判断异常：组件重名
 const componentsList = require('../../components.json')
 if (componentsList[compName]) {
   console.log(
@@ -39,7 +50,7 @@ if (componentsList[compName]) {
   )
 }
 
-// components.json登记组件
+// 4. 文件填充登记组件： /components.json
 componentsList[compName] = `./packages/${compName}/index.js`
 fileSave(path.resolve(__dirname, '../../components.json'))
   .write(JSON.stringify(componentsList, null, '  '), 'utf-8')
@@ -47,7 +58,7 @@ fileSave(path.resolve(__dirname, '../../components.json'))
 
 // packages入口index.js增加组件 - 根据build/build-entry.js自动生成
 
-// index.scss文件
+// 5. 文件填充： /styles/index.scss
 const scssEntryPath = path.join(__dirname, '../../styles/index.scss')
 const scssEntryCont = `${fs.readFileSync(
   scssEntryPath
@@ -56,7 +67,7 @@ fileSave(scssEntryPath)
   .write(scssEntryCont, 'utf-8')
   .end('\n')
 
-// 文本填写内容
+// 文本填写内容：/packages/组件/index.js
 const compEntryFileCont = `import ${compNameCameraCase} from './src/${compName}.vue'
 import 'style/${compName}.scss'
 
@@ -64,8 +75,10 @@ ${compNameCameraCase}.install = app => {
   app.component(${compNameCameraCase}.name, ${compNameCameraCase})
 }
 
-export default ${compNameCameraCase}`,
-  compMainFileCont = `<template>
+export default ${compNameCameraCase}`
+
+// 文本填写内容： /packages/组件/src/组件.vue
+const compMainFileCont = `<template>
   <div class="axe-${compName}">
   </div>
 </template>
@@ -74,13 +87,17 @@ export default ${compNameCameraCase}`,
 export default {
   name: '${compNameCameraCaseAxe}'
 }
-</script>`,
-  compCssFileCont = `@import './mixins/mixins';
+</script>`
+
+// 文本填写内容： /styles/组件.scss
+const compCssFileCont = `@import './mixins/mixins';
 
 @include Block(${compName}) {
   // 组件样式代码写在这里边，写之前将这句注释删掉
-}`,
-  compTestFileCont = `import { expect } from 'chai'
+}`
+
+// 文本填写内容： /tests/unit/组件.spec.js
+const compTestFileCont = `import { expect } from 'chai'
 import ${compNameCameraCaseAxe} from 'packages/${compName}'
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 describe('${compName}测试用例', () => {
@@ -95,29 +112,31 @@ describe('${compName}测试用例', () => {
     let html = app.$el
     expect(html).to.match('${compName}')
   })
-})`,
-  compEntryFiles = [
-    {
-      fileName: 'index.js',
-      content: compEntryFileCont
-    },
-    {
-      fileName: `src/${compName}.vue`,
-      content: compMainFileCont
-    },
-    {
-      fileName: path.join('../../styles', `${compName}.scss`),
-      content: compCssFileCont
-    },
-    {
-      fileName: path.join('../../tests/unit', `${compName}.spec.js`),
-      content: compTestFileCont
-    },
-    {
-      fileName: path.join('../../docs/', compName, 'index.md'),
-      content: `## ${compName} ${compChineseName}`
-    }
-  ]
+})`
+
+// 6. 组合文件填充
+const compEntryFiles = [
+  {
+    fileName: 'index.js',
+    content: compEntryFileCont
+  },
+  {
+    fileName: `src/${compName}.vue`,
+    content: compMainFileCont
+  },
+  {
+    fileName: path.join('../../styles', `${compName}.scss`),
+    content: compCssFileCont
+  },
+  {
+    fileName: path.join('../../tests/unit', `${compName}.spec.js`),
+    content: compTestFileCont
+  },
+  {
+    fileName: path.join('../../docs/', compName, 'index.md'),
+    content: `## ${compName} ${compChineseName}`
+  }
+]
 // 批量创建相关文件
 compEntryFiles.forEach(file => {
   fileSave(path.join(compPath, file.fileName))
