d4cc436bfca7953fefb114355ccbeb5f504f46cc
xing.org1^ <xing.org1@outlook.com>
2021-07-02 21:40:36 +0800

    chore(增加typescript类型检查): typeScript类型支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,38 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@typescript-eslint/parser'
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-multiple-template-root": 0 // https://github.com/vuejs/eslint-plugin-vue/issues/1297
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-multiple-template-root': 0 // https://github.com/vuejs/eslint-plugin-vue/issues/1297
   },
+
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         mocha: true
       }
     }
+  ],
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier',
+    '@vue/typescript'
   ]
-};
+}
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ console.log(AxeUI)
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 // 注册
-let app = createApp(App)
+const app = createApp(App)
 app.use(AxeUI)
 // app.use(AxeIcon)
 app.component('axe-header', Header).component('axe-footer', Footer)
